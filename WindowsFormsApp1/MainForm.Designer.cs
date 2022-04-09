1bb12099d3c502dc501aa735d6f99bc8c68418fd
Илья Погольдин <nice.pogoldin@mail.ru>
2022-04-09 16:13:45 +0400

    картинки и лист

--- WindowsFormsApp1/MainForm.Designer.cs
+++ WindowsFormsApp1/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
---
+++ WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public struct objects
+    {
+        public PictureBox picture;
+        public string name;
+        public Label label;
+        public int price;
+        public string category;
+    }
+    public partial class MainForm : Form
+    {  
+        public List<objects> objList = new List<objects>();
+        public class ReadAllObjects
+        {
+            string[] ReadObj = File.ReadAllLines("Objects.txt");
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+            int x = 30;
+            int y = 10;
+            for(int i = 0; i < objList.Count; i++)
+            {
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
